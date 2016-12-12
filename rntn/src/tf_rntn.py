b5f859b1a4ceee4dcffd3adc6807efd72d9b96ba
SilverGuo <guoyuhan819@gmail.com>
2016-12-12 14:55:36 +0100

    add hidden layer

--- rntn/src/tf_rntn.py
+++ rntn/src/tf_rntn.py
@@ -8,74 +8,75 @@ __all__ = ['RecursiveNTensorN']
 class RecursiveNTensorN():
 
     def __init__(self):
-        self.
+        self.uWeight = 0.0001
+        self.wordSize = 10
+        self.labelNum = 5
 
     # load data, train, test and dev
     def load_data(self, filePath):
         self.lexicon, self.allTree = dataPrep(filePath)
 
+    # input placeholders
+    def add_input_placeholder():
+        self.isLeafPh = tf.placeholder(tf.bool, (None), name='isLeafPh')
+        self.leftChildPh = tf.placeholder(tf.int32, (None), name='leftChildPh')
+        self.rightChildPh = tf.placeholder(tf.int32, (None), name='rightChildPh')
+        self.nodeIndicePh = tf.placeholder(tf.int32, (None), name='nodeIndicePh')
+        self.labelPh = tf.placeholder(tf.int32, (None), name='labelPh')
  
-    # define parameters of rntn
+    # initiate parameters of rntn
     def add_model_variable(self):
-        with tf.name_scope('weights'):
-            self.tensorV = weight_variable([2 * FLAGS.word_size, 
-                                            2 * FLAGS.word_size, 
-                                            FLAGS.word_size], 
-                                           name='tensorV')
-            self.linearW = weight_variable([FLAGS.word_size, 
-                                            2 * FLAGS.word_size], 
-                                            name='linearW')
-            self.softW = weight_variable([FLAGS.label_number, 
-                                         FLAGS.word_size], 
-                                         name='softW')
-        with tf.name_scope('bias'):
-            self.linearB = bias_variables([FLAGS.word_size, 1], 
-                                          name='linearB')
-            self.softB = bias_variables([FLAGS.label_number, 1], 
-                                        name='softB')
+        with tf.variable_scope('embedding'):
+            self.embedding = tf.get_variable(name='wVector', 
+                                             [len(self.lexicon), 
+                                              self.wordSize])
+        with tf.variable_scope('weights'):
+            self.tensorV = tf.get_variable(name='tensorV', 
+                                           [2 * self.wordSize, 
+                                            2 * self.wordSize, 
+                                            self.wordSize])
+            self.linearW = tf.get_variable(name='linearW', 
+                                           [self.wordSize, 
+                                            2 * self.wordSize])
+            self.softW = tf.get_variable(name='softW', 
+                                         [self.labelNum, 
+                                          self.wordSize])
+        with tf.variable_scope('bias'):
+            self.linearB = tf.get_variable(name='linearB', 
+                                           [self.wordSize, 1])
+            self.softB = tf.get_variable(name='softB', 
+                                         [self.labelNum, 1])
+
+    # word vector indice
+    def word_indice(self, wordIndice):
+        return tf.expand_dims(tf.gather(self.embedding, 
+                                        wordIndice), 0)
+
+    # children layer
+    def children_layer(self, leftTensor, rightTensor):
+        return tf.concat(0, [leftTensor, rightTensor])
+
+    # tensor layer
+    def tensor_layer(self, childrenTensor):
+        return tf.matmul(tf.transpose(tf.reshape(tf.matmul(tf.transpose(childrenTensor),
+                                                           tf.reshape(tensorV, 
+                                                                      [self.wordSize * 2, 
+                                                                       self.wordSize * self.wordSize * 2])), 
+                                                 [self.wordSize * 2, self.wordSize])), 
+                         childrenTensor)
+
+    # weight layer
+    def weight_layer(self, childrenTensor):
+        return tf.matmul(linearW, childrenTensor)
+
+    # hidden layer
+    def hidden_layer(self, leftTensor, rightTensor):
+        childrenTensor = children_layer(leftTensor, rightTensor)
+        return tf.nn.relu(weight_layer(self, childrenTensor) + 
+                          tensor_layer(self, childrenTensor) + 
+                          linearB)
+                   
+    # def 
+
 
     def inference(self, sentenceTree, )
-
-# flags of tensorflow
-flags = tf.app.flags
-FLAGS = flags.FLAGS
-flags.DEFINE_float('u_weight', 0.0001, 
-                   'Range for random weight')
-flags.DEFINE_integer('word_size', 10, 
-                     'Word vector size')
-flags.DEFINE_integer('label_number', 5, 
-                     'number of labels')
-
-def weight_variable(shape, name=None):
-    return tf.Variable(tf.random_normal(shape, 0, 
-                                        FLAGS.u_weight), 
-                       name=name)
-
-def bias_variables(shape, name=None):
-    return tf.Variable(tf.zeros(shape, dtype=tf.float32), 
-                       name=name)
-
-def model_train():
-    rntn = tf.VariableScope(False, name='rntn')
-
-    # define parameters of rntn
-    with tf.name_scope('weights'):
-        tensorV = weight_variable([2 * FLAGS.word_size, 
-                                   2 * FLAGS.word_size, 
-                                   FLAGS.word_size], 
-                                  name='tensorV')
-        linearW = weight_variable([FLAGS.word_size, 
-                                   2 * FLAGS.word_size], 
-                                  name='linearW')
-        softW = weight_variable([FLAGS.label_number, 
-                                 FLAGS.word_size], 
-                                name='softW')
-    with tf.name_scope('bias'):
-        linearB = bias_variables([FLAGS.word_size, 1], 
-                                 name='linearB')
-        softB = bias_variables([FLAGS.label_number, 1], 
-                               name='softB')
-
-    # load
-
-    return
