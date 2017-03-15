24aff1e3952a29adcde9e7c9b71f7cc65188b88d
SilverGuo <guoyuhan819@gmail.com>
2017-03-15 18:15:16 +0100

    fire

--- en/lstm/src/sentence_prep.py
+++ en/lstm/src/sentence_prep.py
@@ -9,6 +9,7 @@ class Sentence_Prep(object):
     def __init__(self, w2v_pickle):
         with open(w2v_pickle, 'rb') as f:
             self.word_embedding = pickle.load(f)
+        self.word_idx = self.word_embedding['vocab']['word_idx']
         """
         word_embedding
         - embedding
@@ -17,8 +18,10 @@ class Sentence_Prep(object):
             - word_idx
         """
 
+
     # list of tokens to idx
-    def sentence_encode(self, token_list):
-        pass
+    def sentence_encode(self, token_list, vocab_size=200000, 
+                        load_embedding=False):
+        return [self.word_idx.get(t) for t in token_list]
 
     
